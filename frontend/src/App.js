5b09b116ca97b2cc3be6f459667a57a841a053af
Timothy Yeo <timothyyeo121@gmail.com>
2020-08-27 17:45:34 +0100

    Another hello

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "./components/Navbar"
+import Chat from "./components/Chat"
 
 function App() {
   return (
     <>
     <Navbar />
+    <Chat />
     </>
   );
 }
